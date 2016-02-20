a62f7997918247bb511e4a98f4f1e1248e51a0bc
qicongc <qicongc@cs.cmu.edu>
2016-02-19 19:20:43 -0500

    cnn svm

--- run.med.sh
+++ run.med.sh
@@ -48,3 +48,20 @@ for event in P001 P002 P003; do
   # compute the average precision by calling the mAP package
   ap list/${event}_test_label sift_pred/${event}_pred
 done
+
+echo "#####################################"
+echo "#       MED with CNN Features      #"
+echo "#####################################"
+mkdir -p cnn_pred
+# iterate over the events
+feat_dim_cnn=4096
+for event in P001 P002 P003; do
+  echo "=========  Event $event  ========="
+  # now train a svm model
+  python scripts/train_svm.py $event "cnn/" "feat" "dense" $feat_dim_cnn cnn_pred/svm.$event.model || exit 1;
+  # apply the svm model to *ALL* the testing videos;
+  # output the score of each testing video to a file ${event}_pred 
+  python scripts/test_svm.py cnn_pred/svm.$event.model "cnn/" "feat" "dense" $feat_dim_cnn cnn_pred/${event}_pred || exit 1;
+  # compute the average precision by calling the mAP package
+  ap list/${event}_test_label cnn_pred/${event}_pred
+done
